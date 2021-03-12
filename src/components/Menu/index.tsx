63dea564efdb53f359c54cdc74eff56c27358c18
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-12 17:39:59 -0300

    [CHANGE][ANIMATING CONTAINER COMPONENTS]

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,15 +4,29 @@ import { Feather } from '@expo/vector-icons';
 
 
 import { Container, Code, Nav, NavItem, NavText, SignOutButtonText, SignOutButton } from './style';
+import { Animated } from 'react-native';
 
 // Simple usage, defaults for all but the value
 function Simple() {
   return <SvgQRCode value="https://github.com/gabrielgarciamendonca" size={ 60 } color="#8B10AE" backgroundColor="#FFF" />;
 }
 
-export default function Menu() {
+interface MenuProp {
+  translateY: Animated.Value
+}
+
+export default function Menu(prop:MenuProp) {
   return (
-    <Container>
+    <Container
+      style={
+        {
+          opacity: prop.translateY.interpolate({
+            inputRange: [0, 150],
+            outputRange: [0, 1],
+          })
+        }
+      }
+    >
       <Code>
         <Simple />
       </Code>
--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -2,10 +2,32 @@ import React from 'react';
 
 import { Container, TabsContainer, TabItem, TabText } from './style';
 import { AntDesign } from '@expo/vector-icons';
+import { Animated } from 'react-native';
 
-export default function Tabs() {
+interface TabsProp {
+  translateY: Animated.Value
+}
+
+export default function Tabs(prop: TabsProp) {
   return (
-    <Container>
+    <Container
+      style={
+        {
+          transform: [{
+            translateY: prop.translateY.interpolate({
+              inputRange: [0, 310],
+              outputRange: [0, 40],
+              extrapolate: 'clamp',
+            })
+          }],
+          opacity: prop.translateY.interpolate({
+            inputRange: [0, 310],
+            outputRange: [1, 0.3],
+            extrapolate: 'clamp',
+          })
+        }
+      }
+    >
       <TabsContainer>
         <TabItem>
           <AntDesign name="adduser" size={ 22 } color="#FFF" />
