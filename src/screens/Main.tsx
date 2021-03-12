461bb068544da15af04fc0248b445b50f1f15e20
Gabriel Mendonca <gabriel.dev.lop@gmail.com>
2021-03-12 16:05:53 -0300

    [CHANGE][CREATING MAIN PAGE]

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import Header from '../components/Header';
 import Tabs from '../components/Tabs';
+import { Feather } from '@expo/vector-icons';
 
-import { Container } from './style';
+import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './style';
+import Menu from '../components/Menu';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Menu />
+        <Card>
+          <CardHeader>
+            <Feather name="dollar-sign" size={ 24 } color="#666"/>
+            <Feather name="eye-off" size={ 24 } color="#666"/>
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo Disponível</Title>
+            <Description>R$ 3.500,00</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 20,00 recebida de Gabriel Mendonça hoje às 06:00h.
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
